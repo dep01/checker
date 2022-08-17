d39aa1b58a7df5ad8d25b99cbed514faa2e963e1
dep01 <57895253+dep01@users.noreply.github.com>
2022-08-17 16:25:34 +0700

    name changer and array model

--- src/providers/plagiarismProvider.js
+++ src/providers/plagiarismProvider.js
@@ -5,9 +5,9 @@ import {sys_post_formdata} from '../utils/api_client';
 export async function checkPlagiarism(data={}){
   try {
     
-    // const response = await sys_post_formdata({body:data});
-    // return response.callback;
-    return dummies_checker;
+    const response = await sys_post_formdata({body:data});
+    return response.callback;
+    // return dummies_checker;
   } catch (error) {
     console.log(error.message)
   }
